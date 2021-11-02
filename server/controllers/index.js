2d278512152ac173d465f3108ea05ddf4241c80c
Laura Gusmao <laurargus@gmail.com>
2021-11-02 16:48:16 -0300

    feat: add Tasks controller to index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 const loginController = require('./login');
 const usersController = require('./users');
+const tasksController = require('./tasks');
 
 module.exports = {
   loginController,
   usersController,
+  tasksController,
 };
