91ef71c2a352896d894ef4106a9c12c384577a77
Laura Gusmao <laurargus@gmail.com>
2021-11-05 11:09:08 -0300

    feat: add logout in NavBar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -5,20 +5,29 @@ import logo from '../../assets/isolated-layout.svg';
 import './style.css';
 
 function NavBar() {
+  const logout = () => {
+    localStorage.setItem('todo-token', '');
+  };
+
   return (
     <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <img className="nav-logo" src={ logo } alt="Ebytr logo" />
-        <div className="navbar-brand">Task Manager</div>
-        <div className="collpase navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/tasks" className="nav-link">My Tasks</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/tasks/create" className="nav-link">Create Task</Link>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <img className="nav-logo" src={ logo } alt="Ebytr logo" />
+          <div className="navbar-brand">Task Manager</div>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/tasks" className="nav-link">My Tasks</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/tasks/create" className="nav-link">
+                  Create Task
+                </Link>
+              </li>
+            </ul>
+            <Link className="" to="/" onClick={ logout }>Logout</Link>
+          </div>
         </div>
       </nav>
       <br />
--- client/src/pages/MyTasks/index.js
+++ client/src/pages/MyTasks/index.js
@@ -19,6 +19,7 @@ function MyTasks() {
 
   useEffect(() => {
     getTasks();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
